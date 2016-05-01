8f24790fc0094705be18debe4f3f0ac49a9db3a1
exian <exian@doubledutch.me>
2016-04-30 17:45:53 -0700

    Fix formatting

--- api/main.go
+++ api/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
 )
 
 func main() {
 	server := http.Server{}
-    server.Addr = ":8080"
-    http.HandleFunc("/health", healthHandler)
+	server.Addr = ":8080"
+	http.HandleFunc("/health", healthHandler)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Failed to start server: %s", err)
@@ -17,17 +17,17 @@ func main() {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-    r.Close = true
-    if r.Method != http.MethodGet {
-        w.WriteHeader(http.StatusMethodNotAllowed)
-        header := w.Header()
-        header.Set("Allowed", http.MethodGet)
-        w.Write([]byte(fmt.Sprintf("Bad Request: Method %s not allowed for resource.", r.Method)))
-        return
-    }
-    
-    _, err := w.Write([]byte("OK"))
-    if err != nil {
-        log.Fatalf("Failed to write response: %s", err)   
-    }
+	r.Close = true
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		header := w.Header()
+		header.Set("Allowed", http.MethodGet)
+		w.Write([]byte(fmt.Sprintf("Bad Request: Method %s not allowed for resource.", r.Method)))
+		return
+	}
+
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Fatalf("Failed to write response: %s", err)
+	}
 }
